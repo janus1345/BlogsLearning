a1eeb7c557363a64c82f243e0ec0e9be26ca0895
janus1345 <tanyao1345@163.com>
2020-12-22 00:31:16 +0800

    create relationship_joy_of_life

---
+++ relationship_joy_of_life/README.md
@@ -0,0 +1,3 @@
+## 用python 40行代码读懂庆余年
+> 博客[原地址](https://baijiahao.baidu.com/s?id=1653432154548670845&wfr=spider&for=pc)
+
--- weibo_joy_of_life/character_relationship.py
+++ weibo_joy_of_life/character_relationship.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
 
-import jieba 
+import jieba
 
-joy_of_life_path = ""
-with open(joy _of_life_path, 'r', encoding='utf-8') as file:
-    joy_of_life_txt = file.read()
-jieba.cut(joy_of_life_txt, cut_all=True)
+
+def jieba_cut():
+    """
+    使用jieba中的 全模式 切割了小说
+    :return:
+    """
+    joy_of_life_path = "./novels/joy_of_lift.txt"
+    with open(joy_of_life_path, 'r', encoding='GB18030') as file:
+        joy_of_life_txt = file.read()
+    seg_list = jieba.cut(joy_of_life_txt, cut_all=True, HMM=False)
+    seg_list_strip = [i.strip() for i in seg_list if (i.strip() and
+                                                      (i.strip() not in ["。", "，", "", "，“", "“", "。”",
+                                                                         "：“", ":", "“"]))]
+    print("Full Mode: ", "/".join(seg_list_strip))
+
+
+def synonymous_names(synonymous_dict_path):
+    """
+    使用提前准备好的 人物名称和他们的别名 建立字典
+    :param synonymous_dict_path:
+    :return:
+    """
+    synonymous_dict = {}
+    with open(synonymous_dict_path, 'r', encoding='GB18030') as f:
+        lines = f.readlines()
+    for l in lines:
+        synonymous_dict[l.split(" ")[0]] = l.split(" ")[1]
+    return synonymous_dict
+
+
